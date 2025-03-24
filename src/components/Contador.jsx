6d18038ce817735097d79f81d8a46415de5a4361
Vitor de Lima Domingues <vitorlimadomingues@gmail.com>
2025-03-24 11:18:51 -0300

    Criando as funcionalidades do componente contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Contador = () => {
-  return (
+  
+    // HOOK-useState-Manipula o estado da variável
+    const[contador,setContador]=useState(0);
+
+return (
     <>
     <h1>Meu contador</h1>
+
+    <p>Você clicou: {contador}</p>
+    <button onClick={()=>setContador(contador + 1)}>Aumentar</button>
+    <button onClick={()=>setContador(contador - 1)}>Diminuir</button>
+
 
     </>
   )
